496ff0d3a701f37d01c7b03e3a1b1061a872a475
FelipeLujan <jfelipelujan@gmail.com>
2019-07-12 19:00:54 -0500

    message

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React NOWWW
+          Learn React Today!!!!
         </a>
       </header>
     </div>
