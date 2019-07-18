64d18b70a3427f453ebed9a45843b18b46449c0d
Juan Felipe Lujan <34170261+FelipeLujan@users.noreply.github.com>
2019-07-18 17:32:16 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-Your k8s deploys are now automated!!!.        </p>
+Edit src/App.js and save to reload!!!.        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
