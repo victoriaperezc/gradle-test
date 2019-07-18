e0ff0c4b7f9b645b97c150761674464d11017561
Juan Felipe Lujan <34170261+FelipeLujan@users.noreply.github.com>
2019-07-18 17:14:21 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+your deployments are now automated        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
